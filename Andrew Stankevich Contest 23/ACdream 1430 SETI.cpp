7ab325cae3784edad4ce01944711bdaeb9dad391
烧白君 <rayn1027@outlook.com>
2014-11-06 12:02:23 +0800

    SuffixArray fix

--- Andrew Stankevich Contest 23/ACdream 1430 SETI.cpp
+++ Andrew Stankevich Contest 23/ACdream 1430 SETI.cpp
@@ -45,7 +45,7 @@ public:
 		for (k = 1, p = 1; k < n; k *= 2, m = p) {
 			for (p = 0, i = n - k; i < n; i++) y[p++] = i;
 			for (i = 0; i < n; i++) if (sa[i] >= k) y[p++] = sa[i] - k;
-			for (i = 0; i<m; i++) c[i] = 0;
+			for (i = 0; i < m; i++) c[i] = 0;
 			for (i = 0; i < n; i++) c[x[y[i]]]++;
 			for (i = 1; i < m; i++) c[i] += c[i - 1];
 			for (i = n - 1; i >= 0; i--) sa[--c[x[y[i]]]] = y[i];
